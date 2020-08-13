8eee491e6cd19374702633b83699e349bdaf1bcc
teruhito <teru0516part@yahoo.co.jp>
2020-08-13 13:13:47 +0900

    Report

--- src/models/Report.java
+++ src/models/Report.java
@@ -49,7 +49,7 @@ public class Report {
     @Column(name = "report_date", nullable = false)
     private Date report_date;
 
-    @Column(name = "titile", length = 255, nullable = false)
+    @Column(name = "title", length = 255, nullable = false)
     private String title;
 
     @Lob
